8bcc80897c814faa7edafd589e54441cca812870
juezhan <11991762@qq.com>
2018-03-29 10:41:28 +0800

    第一次提交

---
+++ src/components/FirstPage.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  div.first-page
+    h2 First Page
+</template>
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+<style scoped lang="stylus" type="text/stylus">
+  .first-page
+    h2
+      color #0000ff
+</style>
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import FirstPage from './components/FirstPage'
+
+export default FirstPage
